e10ad9ee4d4f890eac9bb1dfa16143f8fd838ffa
Rohan <hellorhat@gmail.com>
2024-10-19 15:42:38 -0400

    fixed indent

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/vintage-watch-website">
-  <>
-    <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet" />
-    <App />
-  </>
+    <>
+      <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet" />
+      <App />
+    </>
   </BrowserRouter>
 )
