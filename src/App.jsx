91f945131b07e033818bba870abfeb72f27c91cf
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-10-21 18:26:41 -0400

    Added redirectory bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Contents from "./components/Contents"
 import NavBar from "./components/NavBar"
-import SideBar from "./components/SideBar"
+import RedirectoryBar from "./components/RedirectoryBar"
 
 function App() {
 
@@ -8,7 +8,8 @@ function App() {
     <>
       <NavBar />
       <Contents />
-      <SideBar />
+      <RedirectoryBar />
+
     </>
   )
 }
