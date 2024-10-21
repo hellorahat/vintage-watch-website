5a91cc751c5c24f99ce041701e3719460a3d3f20
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-10-21 18:27:33 -0400

    Still testing for how to format properly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ import watch1 from '../assets/pexels-ferarcosn-190819.jpg'
 import watch2 from '../assets/pexels-jatin-anand-33853-125779.jpg'
 import watch3 from '../assets/pexels-joey-nguy-n-1056657-2113994.jpg'
 import '../styles/Carousel.css'
+import '../styles/Home.css'
 import { Link } from 'react-router-dom';
+
 function Home() {
     const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
@@ -19,46 +21,45 @@ function MobileLayout() {
     return (
         <div>
             <h1>Mobile Home</h1>
-            
+
         </div>
     )
 }
 
 function DesktopLayout() {
     return (
-        <><div>
-            <h1>Desktop Home</h1>
-            <Link to="/contact-us" >
-                           test
-                        </Link>
-        </div>
-            <div className="carousel">
-                <Carousel data-bs-theme="dark">
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={watch1}
-                            alt="First slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={watch2}
-                            alt="Second slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item >
-                        <img
-                            className="d-block w-100"
-                            src={watch3}
-                            alt="Third slide" 
-                        />
-                    </Carousel.Item>
-                </Carousel>
-            </div>
-            <div>
-
+        <>
+            <div className="home_container">
+                <div className="test">
+                    <Link to="/contact-us" >
+                        test
+                    </Link>
+                </div>
+                <div className="carousel">
+                    <Carousel data-bs-theme="dark">
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={watch1}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={watch2}
+                                alt="Second slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item >
+                            <img
+                                className="d-block w-100"
+                                src={watch3}
+                                alt="Third slide"
+                            />
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
             </div>
         </>
     )
