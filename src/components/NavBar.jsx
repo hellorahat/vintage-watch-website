9ddbc6d83d94c74f780c1a03256c48874b19de79
Rohan <hellorhat@gmail.com>
2024-10-21 20:05:14 -0400

    corrected capitalization in link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -92,7 +92,7 @@ function DesktopLayout() {
             <nav className="redirectory">
                 <div className="Rcontainer-fluid">
                     <NavLink to="/" className="btn" activeClassName="active">New Arrivals</NavLink>
-                    <NavLink to="/Catalog" className="btn" activeClassName="active">Catalog</NavLink>
+                    <NavLink to="/catalog" className="btn" activeClassName="active">Catalog</NavLink>
                     <NavLink to="/about-us" className="btn" activeClassName="active">About Us</NavLink>
                     <NavLink to="/contact-us" className="btn" activeClassName="active">Contact Us</NavLink>
                 </div>
