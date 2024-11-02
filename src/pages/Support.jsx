b4eecb1f2439c5ca7a5e26b8a0a068f6855d4c8f
JasonL742 <jasonliu741@gmail.com>
2024-11-02 17:59:22 -0400

    added personality to the ai

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -5,13 +5,13 @@ import { MainContainer, ChatContainer, MessageList, Message, MessageInput, Typin
 const API_KEY = import.meta.env.VITE_GptApiKey
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
-  "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
+  "role": "system", "content": "You are an helpful assistant for our ecommerce vintage watch store, we have watches such as cartier, john torcasio, omega hero, casio. You will respond to prompts with the personality of dio from jojo."
 }
 
 function App() {
   const [messages, setMessages] = useState([
     {
-      message: "Hello, I'm ChatGPT! Ask me anything!",
+      message: "Hello, how may I assist you?",
       sentTime: "just now",
       sender: "ChatGPT"
     }
