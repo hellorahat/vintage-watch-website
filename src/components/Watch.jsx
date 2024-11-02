94652ace52363248797cd50245213e350663695b
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-11-02 18:05:26 -0400

    Added to components

---
+++ src/components/Watch.jsx
@@ -0,0 +1,17 @@
+class Watch {
+    constructor({ id, type, brand, model, color, release, diameter, shape, image, price, DateAdded }) {
+        this.id = id;
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.release = release;
+        this.diameter = diameter;
+        this.shape = shape;
+        this.image = image;
+        this.price = price;
+        this.DateAdded = DateAdded;
+    }
+}
+
+export default Watch;
