6017de89b915d27c08b52e69e54a1fb79fe989b1
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-11-01 23:25:30 -0400

    Added caption to each carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,73 +1,63 @@
-import { useMediaQuery } from 'react-responsive';
-import Carousel from 'react-bootstrap/Carousel';
-import watch1 from '../assets/pexels-ferarcosn-190819.jpg'
-import watch2 from '../assets/pexels-jatin-anand-33853-125779.jpg'
-import watch3 from '../assets/pexels-joey-nguy-n-1056657-2113994.jpg'
-import '../styles/Carousel.css'
-import '../styles/Home.css'
+import { useMediaQuery } from "react-responsive";
+import Carousel from "react-bootstrap/Carousel";
+import watch1 from "../assets/carouselWatches/cartier.jpg";
+import watch2 from "../assets/carouselWatches/omega-watches-hero-1.jpg";
+import watch3 from "../assets/carouselWatches/john-torcasio-133Vq4tTpBQ-unsplash.jpg";
+import "../styles/Carousel.css";
+import "../styles/Home.css";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Home() {
-    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
-    return (
-        <>
-            {isMobile ? <MobileLayout /> : <DesktopLayout />}
-        </>
-    )
+  return <>{isMobile ? <MobileLayout /> : <DesktopLayout />}</>;
 }
 
 function MobileLayout() {
-    return (
-        <div>
-            <h1>Mobile Home</h1>
-
-        </div>
-    )
+  return (
+    <div>
+      <h1>Mobile Home</h1>
+    </div>
+  );
 }
 
 function DesktopLayout() {
-    return (
-        <>
-        <div>
+  return (
+    <>
+      <div>
         <Link to="/clock">
-        test<br/>
+          test
+          <br />
         </Link>
-        <Link to="/product">
-        test2
-        </Link>
+        <Link to="/product">test2</Link>
+      </div>
+      <div className="home_container">
+        <div className="carousel">
+          <Carousel data-bs-theme="dark">
+            <Carousel.Item>
+              <img className="d-block w-100" src={watch1} alt="First slide" />
+              <Carousel.Caption>
+                <Link to="/catalog">Cartier</Link>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={watch2} alt="Second slide" />
+              <Carousel.Caption>
+                <Link to="/catalog">Omega</Link>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={watch3} alt="Third slide" />
+              <Carousel.Caption>
+                <Link to="/catalog">Rolex</Link>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </div>
-            <div className="home_container">
-                <div className="carousel">
-                    <Carousel data-bs-theme="dark">
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={watch1}
-                                alt="First slide"
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={watch2}
-                                alt="Second slide"
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item >
-                            <img
-                                className="d-block w-100"
-                                src={watch3}
-                                alt="Third slide"
-                            />
-                        </Carousel.Item>
-                    </Carousel>
-                </div>
-            </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
-
-export default Home
+export default Home;
