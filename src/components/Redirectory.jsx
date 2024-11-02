8e50ef2a3c0d9d8c9f2bfbc545d98e0c29fd826a
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-11-01 23:28:39 -0400

    No changes made

--- src/components/Redirectory.jsx
+++ src/components/Redirectory.jsx
@@ -1,17 +1,25 @@
-import { NavLink } from 'react-router-dom';
-import '../styles/RedirectoryBar.css'
+import { NavLink } from "react-router-dom";
+import "../styles/RedirectoryBar.css";
 
 function Redirectory() {
-    return (
-        <nav className="redirectory">
-            <div className="Rcontainer-fluid">
-                <NavLink to="/" className="btn" activeClassName="active">New Arrivals</NavLink>
-                <NavLink to="/catalog" className="btn" activeClassName="active">Catalog</NavLink>
-                <NavLink to="/about-us" className="btn" activeClassName="active">About Us</NavLink>
-                <NavLink to="/contact-us" className="btn" activeClassName="active">Contact Us</NavLink>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="redirectory">
+      <div className="Rcontainer-fluid">
+        <NavLink to="/" className="btn" activeClassName="active">
+          New Arrivals
+        </NavLink>
+        <NavLink to="/catalog" className="btn" activeClassName="active">
+          Catalog
+        </NavLink>
+        <NavLink to="/about-us" className="btn" activeClassName="active">
+          About Us
+        </NavLink>
+        <NavLink to="/contact-us" className="btn" activeClassName="active">
+          Contact Us
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
 
-export default Redirectory
+export default Redirectory;
