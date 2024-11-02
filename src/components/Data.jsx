30c52934af29b9ca93308ca1f41a1937aedaef03
BrianAbreuFilpo <brianfilpo99@gmail.com>
2024-11-02 15:29:16 -0400

    Integrated Sidebar

---
+++ src/components/Data.jsx
@@ -0,0 +1,8 @@
+import Watch from './Watch';
+
+export const watches = [
+    new Watch("Men's - Vintage", "Rolex", "Rolex Pepsi", "Blue", "1954", 40, "Round"),
+    new Watch(2, "Omega Speedmaster", "Omega", 1965, "Analog", 1965, "Used"),
+    new Watch(3, "Apple Watch Series 8", "Apple", 399, "Smartwatch", 2022, "New"),];
+
+constructor(type, brand, model, color, release, diameter, shape) 
