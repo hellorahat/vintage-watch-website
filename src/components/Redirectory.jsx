e76167b4d334aaa639e1de199d8c1e3cc7fdc7fd
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-11-03 01:21:48 -0500

    Fixed the redirectory bar to properly link the homepage

--- src/components/Redirectory.jsx
+++ src/components/Redirectory.jsx
@@ -1,22 +1,22 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../styles/RedirectoryBar.css";
 
 function Redirectory() {
   return (
     <nav className="redirectory">
       <div className="Rcontainer-fluid">
-        <NavLink to="/" className="btn" activeClassName="active">
+        <Link to="//" className="btn" activeClassName="active">
           New Arrivals
-        </NavLink>
-        <NavLink to="/catalog" className="btn" activeClassName="active">
+        </Link>
+        <Link to="/catalog" className="btn" activeClassName="active">
           Catalog
-        </NavLink>
-        <NavLink to="/about-us" className="btn" activeClassName="active">
+        </Link>
+        <Link to="/about-us" className="btn" activeClassName="active">
           About Us
-        </NavLink>
-        <NavLink to="/contact-us" className="btn" activeClassName="active">
+        </Link>
+        <Link to="/contact-us" className="btn" activeClassName="active">
           Contact Us
-        </NavLink>
+        </Link>
       </div>
     </nav>
   );
