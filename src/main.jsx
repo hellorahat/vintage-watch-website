3b07f18c3924ea313073b6f8fcc4f7d73ca1ad03
Rohan <hellorhat@gmail.com>
2024-11-03 18:36:07 -0500

    added favoritesprovider

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,19 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter } from 'react-router-dom'
 import { FavoritesProvider } from './utility/FavoritesContext.jsx'
+import { AlertProvider } from './utility/AlertContext.jsx'
+import AlertComponent from './components/AlertComponent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/vintage-watch-website">
-    <FavoritesProvider>
     <>
-      <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet" />
-      <App />
+      <AlertProvider>
+        <FavoritesProvider>
+          <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet" />
+          <App />
+          <AlertComponent />
+        </FavoritesProvider>
+      </AlertProvider>
     </>
-    </FavoritesProvider>
   </BrowserRouter>
 )
