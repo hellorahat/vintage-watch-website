07ab5896c502a814ff3ec9c04c9668a5006f5de0
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-11-03 16:53:44 -0500

    Made the classes for the watch model, brand, price

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -79,8 +79,8 @@ function Catalog() {
     setFilteredWatches(filtered);
   };
 
-  if(loading) {
-    return <div>Loading...</div>
+  if (loading) {
+    return <div>Loading...</div>;
   }
 
   return (
@@ -96,9 +96,9 @@ function Catalog() {
                 src={retrieveSource(watch.image)}
                 alt={watch.model}
               />
-              <h3 className="watch-brand">{watch.brand}</h3>
-              <h4>{watch.model}</h4>
-              <p>Price: {watch.price}</p>
+              <h5 className="watch-brand">{watch.brand}</h5>
+              <h6 className="watch-model">{watch.model}</h6>
+              <h4 className="watch-price">${watch.price}</h4>
               <div
                 className="like-button"
                 onClick={() => addToFavorites(watch)}
@@ -113,9 +113,9 @@ function Catalog() {
                   <path
                     d="M53.5733 6.29333C52.2113 4.93068 50.5942 3.84972 48.8143 3.11222C47.0344 2.37473 45.1266 1.99513 43.2 1.99513C41.2734 1.99513 39.3656 2.37473 37.5857 3.11222C35.8058 3.84972 34.1887 4.93068 32.8267 6.29333L30 9.12L27.1733 6.29333C24.4221 3.54216 20.6907 1.99656 16.8 1.99656C12.9092 1.99656 9.17784 3.54216 6.42666 6.29333C3.67548 9.04451 2.12988 12.7759 2.12988 16.6667C2.12988 20.5574 3.67548 24.2888 6.42666 27.04L30 50.6133L53.5733 27.04C54.936 25.678 56.0169 24.0608 56.7544 22.2809C57.4919 20.5011 57.8715 18.5933 57.8715 16.6667C57.8715 14.74 57.4919 12.8323 56.7544 11.0524C56.0169 9.2725 54.936 7.65535 53.5733 6.29333Z"
                     stroke="#1E1E1E"
-                    stroke-width="4"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="4"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                 </svg>
               </div>
