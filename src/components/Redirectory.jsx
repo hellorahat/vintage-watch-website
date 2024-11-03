b89879026b772c03794e120b90ff48fee6585c0f
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-11-03 01:57:08 -0500

    Made the active homepage have a illuminated bar

--- src/components/Redirectory.jsx
+++ src/components/Redirectory.jsx
@@ -1,20 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/RedirectoryBar.css";
 
 function Redirectory() {
+  const location = useLocation();
+
   return (
     <nav className="redirectory">
       <div className="Rcontainer-fluid">
-        <Link to="//" className="btn" activeClassName="active">
+        <Link
+          to="//"
+          className={`btn ${location.pathname === "/" ? "active" : ""}`}
+        >
           New Arrivals
         </Link>
-        <Link to="/catalog" className="btn" activeClassName="active">
+        <Link
+          to="/catalog"
+          className={`btn ${location.pathname === "/catalog" ? "active" : ""}`}
+        >
           Catalog
         </Link>
-        <Link to="/about-us" className="btn" activeClassName="active">
+        <Link
+          to="/about-us"
+          className={`btn ${location.pathname === "/about-us" ? "active" : ""}`}
+        >
           About Us
         </Link>
-        <Link to="/contact-us" className="btn" activeClassName="active">
+        <Link
+          to="/contact-us"
+          className={`btn ${
+            location.pathname === "/contact-us" ? "active" : ""
+          }`}
+        >
           Contact Us
         </Link>
       </div>
