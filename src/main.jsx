1ccda9b57948e3c18a6406ff2668c1ddbcbe4dfb
Rohan <hellorhat@gmail.com>
2024-11-03 19:20:15 -0500

    added cart context

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,19 @@ import { BrowserRouter } from 'react-router-dom'
 import { FavoritesProvider } from './utility/FavoritesContext.jsx'
 import { AlertProvider } from './utility/AlertContext.jsx'
 import AlertComponent from './components/AlertComponent.jsx'
+import { CartProvider } from './utility/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/vintage-watch-website">
     <>
       <AlertProvider>
-        <FavoritesProvider>
-          <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet" />
-          <App />
-          <AlertComponent />
-        </FavoritesProvider>
+        <CartProvider>
+          <FavoritesProvider>
+            <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet" />
+            <App />
+            <AlertComponent />
+          </FavoritesProvider>
+        </CartProvider>
       </AlertProvider>
     </>
   </BrowserRouter>
