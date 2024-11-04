3a75737db9ac6100f46ae5a285820c1ea7bf87b8
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-11-03 21:30:26 -0500

    Made the route work for the product

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Home from '../pages/Home';
+import Home from "../pages/Home";
 import Catalog from "../pages/Catalog";
 import AboutUs from "../pages/AboutUs";
 import ContactUs from "../pages/ContactUs";
-import Favorites from "../pages/Favorites"
-import Cart from "../pages/Cart"
-import Product from "../pages/product"
-import Support from "../pages/Support"
-import '../styles/Contents.css'
+import Favorites from "../pages/Favorites";
+import Cart from "../pages/Cart";
+import Product from "../pages/product";
+import Support from "../pages/Support";
+import "../styles/Contents.css";
 
 function Contents() {
   return (
-    <div className='contents'>
+    <div className="contents">
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/catalog' element={<Catalog />}></Route>
-        <Route path='/about-us' element={<AboutUs />}></Route>
-        <Route path='/contact-us' element={<ContactUs />}></Route>
-        <Route path='/favorites' element={<Favorites />}></Route>
-        <Route path='/cart' element={<Cart />}></Route>
-        <Route path='/product' element={<Product />}></Route>
-        <Route path='/support' element={<Support />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/catalog" element={<Catalog />}></Route>
+        <Route path="/about-us" element={<AboutUs />}></Route>
+        <Route path="/contact-us" element={<ContactUs />}></Route>
+        <Route path="/favorites" element={<Favorites />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/product/:id" element={<Product />}></Route>
+        <Route path="/support" element={<Support />}></Route>
       </Routes>
     </div>
   );
