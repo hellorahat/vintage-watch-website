fab7e8c15f4f9a344788772432897bc67e6c8342
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-11-03 21:31:03 -0500

    Added the link to press each product

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,8 @@ import { doc, setDoc, collection } from "firebase/firestore";
 import { loadAllImages, retrieveSource } from "../utility/retrieveSource.jsx";
 import { useFavorites } from "../utility/FavoritesContext.jsx";
 import { useAlerts } from "../utility/AlertContext.jsx";
-import MyChatBot from './MyChatBot';
+import MyChatBot from "./MyChatBot";
+import "../styles/Product.css";
 function Home() {
   const [watches, setWatches] = useState([]);
   const [filteredWatches, setFilteredWatches] = useState([]);
@@ -38,10 +39,10 @@ function Home() {
     fetchImages();
   }, []);
 
-  if(loading) {
-    return <div>Loading...</div>
+  if (loading) {
+    return <div>Loading...</div>;
   }
-  
+
   return (
     <>
       <div className="home_container">
@@ -78,47 +79,49 @@ function Home() {
           {filteredWatches.length > 0 ? (
             filteredWatches.map((watch) => (
               <div key={watch.id} className="watch-card">
-                <img
-                  className="watch-image"
-                  src={retrieveSource(watch.image)}
-                  alt={watch.model}
-                />
-                <h3 className="watch-brand">{watch.brand}</h3>
-                <h4>{watch.model}</h4>
-                <p>Price: {watch.price}</p>
-                <div
-                  className="like-button"
-                  onClick={() => {
-                    const exists = favorites.some(favorite => favorite.id === watch.id);
-                    if(!exists) addAlert("Added to Favorites!");
-                    addFavorite(watch);
-                  }}
-                >
-                  <svg
-                    width="25"
-                    height="25"
-                    viewBox="0 0 60 53"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                <Link className="wrap-product" to={`/product/${watch.id}`}>
+                  <img
+                    className="watch-image"
+                    src={retrieveSource(watch.image)}
+                    alt={watch.model}
+                  />
+                  <h3 className="watch-brand">{watch.brand}</h3>
+                  <h4>{watch.model}</h4>
+                  <p>Price: {watch.price}</p>
+                  <div
+                    className="like-button"
+                    onClick={() => {
+                      const exists = favorites.some(
+                        (favorite) => favorite.id === watch.id
+                      );
+                      if (!exists) addAlert("Added to Favorites!");
+                      addFavorite(watch);
+                    }}
                   >
-                    <path
-                      d="M53.5733 6.29333C52.2113 4.93068 50.5942 3.84972 48.8143 3.11222C47.0344 2.37473 45.1266 1.99513 43.2 1.99513C41.2734 1.99513 39.3656 2.37473 37.5857 3.11222C35.8058 3.84972 34.1887 4.93068 32.8267 6.29333L30 9.12L27.1733 6.29333C24.4221 3.54216 20.6907 1.99656 16.8 1.99656C12.9092 1.99656 9.17784 3.54216 6.42666 6.29333C3.67548 9.04451 2.12988 12.7759 2.12988 16.6667C2.12988 20.5574 3.67548 24.2888 6.42666 27.04L30 50.6133L53.5733 27.04C54.936 25.678 56.0169 24.0608 56.7544 22.2809C57.4919 20.5011 57.8715 18.5933 57.8715 16.6667C57.8715 14.74 57.4919 12.8323 56.7544 11.0524C56.0169 9.2725 54.936 7.65535 53.5733 6.29333Z"
-                      stroke="#1E1E1E"
-                      strokeWidth="4"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </div>
+                    <svg
+                      width="25"
+                      height="25"
+                      viewBox="0 0 60 53"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M53.5733 6.29333C52.2113 4.93068 50.5942 3.84972 48.8143 3.11222C47.0344 2.37473 45.1266 1.99513 43.2 1.99513C41.2734 1.99513 39.3656 2.37473 37.5857 3.11222C35.8058 3.84972 34.1887 4.93068 32.8267 6.29333L30 9.12L27.1733 6.29333C24.4221 3.54216 20.6907 1.99656 16.8 1.99656C12.9092 1.99656 9.17784 3.54216 6.42666 6.29333C3.67548 9.04451 2.12988 12.7759 2.12988 16.6667C2.12988 20.5574 3.67548 24.2888 6.42666 27.04L30 50.6133L53.5733 27.04C54.936 25.678 56.0169 24.0608 56.7544 22.2809C57.4919 20.5011 57.8715 18.5933 57.8715 16.6667C57.8715 14.74 57.4919 12.8323 56.7544 11.0524C56.0169 9.2725 54.936 7.65535 53.5733 6.29333Z"
+                        stroke="#1E1E1E"
+                        strokeWidth="4"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </div>
+                </Link>
               </div>
             ))
           ) : (
             <p>No watches found for the selected date.</p>
           )}
         </div>
-      
       </div>
-
     </>
   );
 }
